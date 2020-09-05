add65b8f34ec80080286b0e9527c841824258745
hp <hp@DESKTOP-3CUG606>
2020-09-05 09:59:54 +0530

    commit by lanke

--- GitHubProgram1/src/com/gitdemo/DemoGitHub.java
+++ GitHubProgram1/src/com/gitdemo/DemoGitHub.java
@@ -1,6 +1,10 @@
 package com.gitdemo;
 
 public class DemoGitHub {
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello main method");
