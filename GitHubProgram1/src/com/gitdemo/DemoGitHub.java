a1999a5184be297b53b7377e5bba60f04f484c36
Patil1212 <70788901+Patil1212@users.noreply.github.com>
2020-09-07 14:17:21 +0530

    Update DemoGitHub.java

--- GitHubProgram1/src/com/gitdemo/DemoGitHub.java
+++ GitHubProgram1/src/com/gitdemo/DemoGitHub.java
@@ -5,6 +5,10 @@ public class DemoGitHub {
 	{
 		System.out.println("m1 method");
 	}
+	public void m2()
+	{
+		System.out.println("m2 method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello main method");
